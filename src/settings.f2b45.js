6409deb7cd257e627f03302fbb04fedab6443369
Kent <kent.lin@primestar.plus>
2023-10-18 15:44:47 +0800

    spine與音效

---
+++ src/settings.f2b45.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/Main.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",resources:"8c664",main:"66a13"}};
