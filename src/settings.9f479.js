59478c7abab2bfa454c915ff69b6d85aeaaea26b
Kent <>
2023-12-25 11:05:29 +0800

    .

---
+++ src/settings.9f479.js
@@ -0,0 +1,28 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/scene/Main.fire",
+    orientation: "",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "cb4f5",
+        resources: "3c43f",
+        audio: "4099d",
+        prefab: "2d3e3",
+        spine: "b1fe9",
+        texture: "c58cb",
+        main: "bd1f2"
+    }
+};
