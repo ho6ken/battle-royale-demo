7528abee92ab8cb1604b702b5c087cf60c755b9b
Kent <>
2023-12-25 12:44:42 +0800

    .

---
+++ src/settings.495b0.js
@@ -0,0 +1,28 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/scene/New Scene.fire",
+    orientation: "",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "cb4f5",
+        resources: "3c43f",
+        audio: "4099d",
+        prefab: "c34ee",
+        spine: "28c7a",
+        texture: "c01ef",
+        main: "435b9"
+    }
+};
