7e3f489aea87e9dbd96b361639ab704506c274dc
Kent <>
2023-12-25 10:24:30 +0800

    open debug tool

---
+++ src/settings.53f43.js
@@ -0,0 +1,28 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/scene/Main.fire",
+    orientation: "",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "cb4f5",
+        resources: "3c43f",
+        audio: "4099d",
+        prefab: "aa962",
+        spine: "7c227",
+        texture: "4cf15",
+        main: "32a1a"
+    }
+};
